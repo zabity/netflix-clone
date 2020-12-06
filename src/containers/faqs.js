f1b21b5b6d1931bcc258494391ca6e1a85c00bb4
zabity <scruel@o2.pl>
2020-12-07 00:19:21 +0100

    footer and accordion

---
+++ src/containers/faqs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Accordion } from '../components';
+import faqData from '../fixtures/faqs';
+
+export function FaqsContainer(){
+  return(
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqData.map( item => (
+        <Accordion.Item key={item.id}>
+          <Accordion.Header>{item.header}</Accordion.Header>
+          <Accordion.Body>{item.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
+}
